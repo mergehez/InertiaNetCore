77ea0e504f36d49687e24664edcbde020a62112f
mergehez <argesozdogan@gmail.com>
2024-10-01 01:30:06 +0200

    code refactoring

--- InertiaNetCore.Demo/Frontend/Vue/vite.config.ts
+++ InertiaNetCore.Demo/Frontend/Vue/vite.config.ts
@@ -2,32 +2,34 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import path from 'path';
 import laravel from 'laravel-vite-plugin';
+import { mkdirSync } from 'node:fs';
 
 const outDir = '../../wwwroot/build';
+mkdirSync(outDir, { recursive: true });
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['src/app.ts', 'src/app.scss'],
             publicDirectory: outDir,
-            refresh: true
+            refresh: true,
         }),
         vue({
             template: {
                 transformAssetUrls: {
                     base: null,
-                    includeAbsolute: false
-                }
-            }
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
     build: {
         outDir,
-        emptyOutDir: true
-    }
+        emptyOutDir: true,
+    },
 });
