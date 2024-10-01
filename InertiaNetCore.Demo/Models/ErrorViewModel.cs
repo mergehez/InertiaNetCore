42f4a07bbde47d184535ed89ec007d4eb31be2d2
mergehez <argesozdogan@gmail.com>
2024-10-01 02:11:59 +0200

    deleted unused class ErrorViewModel

--- InertiaNetCore.Demo/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace InertiaNetCore.Demo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
